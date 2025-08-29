0789eb430901b5167db04c6eaeacf2f20aa35404
Евгений Лактионов <laktionov_e@t.top-academy.ru>
2025-08-29 12:06:34 +0300

    Input error handling

--- ConsoleCalcualator01.cpp
+++ ConsoleCalcualator01.cpp
@@ -1,18 +1,39 @@
 ﻿#include <iostream>
+#include <print>
 #include <clocale>
 using namespace std;
+
+void errorMessage(const string& message);
 
 int main() {
     setlocale(LC_ALL, "ru");
 
-    int a, b;
-    cout << "Введи первое число: ";
-    cin >> a;
-    cout << "Введи второе число: ";
-    cin >> b;
-    cout << "Сумма: " << a + b << endl;
+    std::print("Введи первое число: ");
+    double a;
+    if (!(cin >> a)) {
+        errorMessage("Ошибка: введено не число");
+        return 1;
+    }
 
-    cin.get(); // чтобы поймать Enter после второго числа
-    cin.get(); // ожидание ввода
+    std::print("Введи второе число: ");
+    double b;
+    if (!(cin >> b)) {
+        errorMessage("Ошибка: введено не число");
+        return 1;
+    }
+
+    std::println("{} + {} = {}", a, b, a + b);
+
+    cin.get();
+    cin.get();
     return 0;
 }
+
+void errorMessage(const string& message)
+{
+    std::println("{}", message);
+    cin.clear();
+    cin.ignore(1000, '\n');
+    cin.get();
+}
+
